dbb2161c53228c53de99a25091d485506d5fb2ec
rotem nevgauker <nevgauker@gmail.com>
2017-09-28 13:39:52 +0100

    Added a custume user profile,a manager and the migrations for our database.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+class userPofileManager(BaseUserManager):
+    """Help django works with our custume user model."""
+    def create_user(self,email,name,password=None):
+        """Create a new user profile."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_super_user(self,email,name,password):
+        """Creates and saves a new super user with given details"""
+        user = create_user(email,name,password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class userProfile(AbstractBaseUser,PermissionsMixin):
+    """Represent a "user profile"  in our system."""
+
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = userPofileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['name']
+
+    def get_full_name(self):
+        """Used to get a users full name."""
+        return self.name
+
+    def get_short_name(self):
+        """ Used to get ausers short name."""
+        return self.name
+    def __str__(self):
+        """Django uses this whenit need to convert object to a string."""
+        return self.email
