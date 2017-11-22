7fd718d75dcf708344673b0b541466e767983720
rotem nevgauker <nevgauker@gmail.com>
2017-11-22 11:27:37 +0200

    update the url  back to my.

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/LondonAppDev/byob-profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/nevgauker/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
